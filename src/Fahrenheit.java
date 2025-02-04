c95a38c93dc9a9a965f330c33c1917b58086400b
Borges <carlosmateus7720@gmail.com>
2025-02-04 13:24:17 -0300

    mudancas no codigo feitas
    codigo 0

--- src/Fahrenheit.java
+++ src/Fahrenheit.java
@@ -2,14 +2,12 @@ public class Fahrenheit {
     public static void main(String[] args) {
         double tempCelsius = 30;
 
-        double tempFaheranheit = 1;
-        System.out.println("Temperatura em Faheranheit" + tempFaheranheit);
 
-        double formula = (tempFaheranheit * 1.8) +32;
+        double faheFormula = (tempCelsius * 1.8) +32;
 
-        int resultadoFormula = (int) formula;
-
+        int resultadoFormula = (int) faheFormula;
         System.out.println(resultadoFormula);
+
 
     }
 
